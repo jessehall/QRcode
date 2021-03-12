793b5f4085342eb5c5549b4269d58d7e4f80f406
test <test@test.com>
2021-03-12 15:34:23 -0800

    1st step to dynamic

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script>
 	import QrCode from "svelte-qrcode";
 	let domain = "https://www.swervecreative.com";
+	let name = 'jesse';
+	let myDomain = "";
+	function handleSubmit(e) {
+		
+		myDomain = e;
+		console.log(myDomain);
+		
+	}
+
 </script>
 
 
@@ -11,9 +20,16 @@
 			Dynamically Generate a QR code:
 		</h1>
 		<p>Type in a URL and hit enter to generat a QR code:</p>
-		<input value={domain} size="50"><br>
-	  <QrCode value={domain} />
+		<input bind:value={domain} size="50"><button on:click={handleSubmit(domain)}>New</button><br>
+		{#if myDomain}
+	  		<QrCode value={myDomain} />
+			  <script>myDomain = "";</script>
+	  	{/if}
+	  
 	</div>
+
+	<!-- <input bind:value={domain}> -->
+	<h1>Hello {domain}!</h1>
 </main>
 
 <style>
