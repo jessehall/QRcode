a4b4e0d253ab424b66054ebb17f3684c0dfaf2f4
test <test@test.com>
2021-03-12 14:19:32 -0800

    first try at QR code generation

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import QrCode from "svelte-qrcode";
+	let domain = "https://www.swervecreative.com";
+</script>
+
+
+
+<main>
+	<div class="container">
+		<h1>
+			Dynamically Generate a QR code:
+		</h1>
+		<p>Type in a URL and hit enter to generat a QR code:</p>
+		<input value={domain} size="50"><br>
+	  <QrCode value={domain} />
+	</div>
+</main>
+
+<style>
+	
+</style>
---
+++ src/helpers/QRJS.svelte
@@ -0,0 +1,25 @@
+let export qrcode = new QRCode("qrcode");
+
+function makeCode () {		
+	let export elText = document.getElementById("text");
+	
+	if (!elText.value) {
+		alert("Input a text");
+		elText.focus();
+		return;
+	}
+	
+	qrcode.makeCode(elText.value);
+}
+
+makeCode();
+
+$("#text").
+	on("blur", function () {
+		makeCode();
+	}).
+	on("keydown", function (e) {
+		if (e.keyCode == 13) {
+			makeCode();
+		}
+	});
